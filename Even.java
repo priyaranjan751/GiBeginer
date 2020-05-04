eeae9b252076507aad0690dca0a547486467728a
priya <priyastyle751.com>
2020-05-04 21:17:10 +0530

    all files commited

---
+++ Even.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,c=0;
+		for(i=1;i<=10;i++)
+		{
+			if(i%2==0)
+				c++;
+		}
+		System.out.println("Count of odd numbers between 1 to 10 is "+c);
+	}
+
+}
