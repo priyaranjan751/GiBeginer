55d05adc029b13aacd61ed8f95fa804530e49f70
priya <priyastyle751.com>
2020-05-05 22:52:52 +0530

    Added two lines in even.java

--- Even.java
+++ Even.java
@@ -8,7 +8,10 @@ public class Even {
 		for(i=1;i<=10;i++)
 		{
 			if(i%2==0)
+			{
 				c++;
+				System.out.print(i);
+			}
 		}
 		System.out.println("Count of odd numbers between 1 to 10 is "+c);
 	}
